c18eab03cc372e0da1c30915082820137808fec5
yon969@gmail.com <yon969@gmail.com>
2022-08-28 14:27:01 +0000

    Created Create_Simulation, Train_Model and Sidebar; Edited settings; Deleted Model_Training_Input and Simulation_Input

---
+++ client_code/Sidebar.py
@@ -0,0 +1,26 @@
+from ._anvil_designer import SidebarTemplate
+from anvil import *
+from .Create_Simulation import Create_Simulation
+from .Train_Model import Train_Model
+
+class Sidebar(SidebarTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.create_simulation_lnk_click()
+    # Any code you write here will run before the form opens.
+  
+    self.create_simulation_lnk.tag.form_to_open = Create_Simulation()
+    self.train_model_lnk.tag.form_to_open = Train_Model()
+
+  def navigation_click(self, **event_args):
+    form_to_open = event_args['sender'].tag.form_to_open
+    self.content_panel.clear()
+    self.content_panel.add_component(form_to_open)
+    pass
+
+
+
+
+
+
