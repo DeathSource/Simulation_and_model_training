3d234df013592e83e0c452425e02bde2c76384b9
yon969@gmail.com <yon969@gmail.com>
2022-08-28 15:16:29 +0000

    Edited Create_Simulation, Train_Model, settings, Sidebar and theme/parameters.yaml

--- client_code/Sidebar.py
+++ client_code/Sidebar.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import SidebarTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 from .Create_Simulation import Create_Simulation
 from .Train_Model import Train_Model
 
--- client_code/Train_Model/__init__.py
+++ client_code/Train_Model/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import Train_ModelTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class Train_Model(Train_ModelTemplate):
   def __init__(self, **properties):
