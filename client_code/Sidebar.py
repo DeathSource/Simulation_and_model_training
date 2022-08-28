ee666aaf3eedc5fffd2aff25a69380346a97c592
yon969@gmail.com <yon969@gmail.com>
2022-08-28 14:51:16 +0000

    Edited Sidebar

--- client_code/Sidebar.py
+++ client_code/Sidebar.py
@@ -9,6 +9,7 @@ class Sidebar(SidebarTemplate):
     self.init_components(**properties)
     self.create_simulation_lnk.tag.form_to_open = Create_Simulation()
     self.train_model_lnk.tag.form_to_open = Train_Model()
+    self.content_panel.add_component(Create_Simulation())
     
     # Any code you write here will run before the form opens.
   def navigation_click(self, **event_args):
