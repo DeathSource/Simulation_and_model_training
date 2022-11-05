3498afc5e58f22947d8b3a44c57cd83b5cd8adf3
yon969@gmail.com <yon969@gmail.com>
2022-11-05 21:57:57 +0000

    Edited Create_Simulation and Sidebar

--- client_code/Create_Simulation.py
+++ client_code/Create_Simulation.py
@@ -9,10 +9,13 @@ class Create_Simulation(Create_SimulationTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+    self.initial_condition_dg.
 
     # Any code you write here will run before the form opens.
 
   def start_simulation_btn_click(self, **event_args):
     # get initial position and velocity of each body
-    
+    pass
+
+
 
